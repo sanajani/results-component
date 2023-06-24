9066fd1e59643315ef60d4ffe8f68fd4c2aa8726
Sanaullah <webdevfarsi@gmail.com>
2023-06-24 11:04:17 +0430

    new letter page finish

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,26 +1,37 @@
 const button = document.getElementById("btn");
 const inputValue = document.getElementById("email");
-const error = document.querySelector(".error");
+const small = document.querySelector(".small");
 const container = document.querySelector(".container");
 const confirms = document.querySelector(".confirm");
+const dismiss = document.getElementById('dismiss');
 
 let emailValidation = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+inputValue.addEventListener('input',(e) => {
+  
+    if(!e.target.value.match(emailValidation)){
+        small.classList.add('error')
+    }else{
+        small.classList.remove('error')
+    }
+})
+
 button.addEventListener("click",(e) => {
     e.preventDefault();
     if(!inputValue.value){
-        error.style.display = 'inline-block'
-        inputValue.style.borderColor = 'red'
-        error.innerHTML = "Email filed is required"
+        small.classList.add('error')
+        inputValue.classList.add('error')
     }else{
-        error.style.display = 'none'
-        inputValue.style.borderColor = `gray`
+        small.classList.remove('error')
+        inputValue.classList.remove('error')
     }
     if(inputValue.value.match(emailValidation)){
         container.style.display = 'none'
         confirms.style.display = 'block'
-
-
-    }else{
-        alert("Not Match")
     }
+})
+
+dismiss.addEventListener("click",() => {
+    container.style.display = 'block'
+    confirms.style.display = 'none'
 })
